4e7b6dfbf39851fd997e64bbbe8843af0e6ea442
Ashley Wong <tofudragon333@gmail.com>
2022-06-29 01:06:23 -0400

    commit tree bb4894d7ed5be3d7b1693db2ed8fdeaa41d1cd3d

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def happy_new_year
-  # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+    if counter == 0
+      puts "Happy New Year!"
+    end
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +23,26 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
+  (1..100).each do |num|
+    if num % 5 == 0 && num % 3 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+     puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      num
+    end
+  end
   # your code here
 end
 
 def reverse_string(str)
-  # your code here
+  reverse_str = ""
+  i=0
+  while i< str.length
+    reverse_str = str[i] + reverse_str
+  end
+  reverse_str
+
 end
